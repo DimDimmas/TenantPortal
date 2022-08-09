331bf0ad34b562138147f4494598108300e4d80f
Rahmatulah Sidik <blcak.gun123@gmail.com>
2022-08-09 13:37:51 +0700

    add btn print datatable

--- resources/views/tracking_loading/report_summary/index.blade.php
+++ resources/views/tracking_loading/report_summary/index.blade.php
@@ -66,10 +66,22 @@
 @endsection
 
 @push('scripts')
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
 <script>
 function getDataTable(dateSelected){
     $('#tableList').DataTable({
       order: [[0, 'desc']],
+      dom: 'Bfrtip',
+      buttons: [
+          { extend: 'copyHtml5', text: 'Copy', className: 'btn btn-sm btn-info' },
+          { extend : 'excelHtml5', text: 'Excell', className: 'btn btn-sm btn-success' },
+          { extend : 'csvHtml5', text: 'CSV', className: 'btn btn-sm btn-success' },
+          { extend : 'pdfHtml5', text: 'PDF', className: 'btn btn-sm btn-danger' }
+      ],
       processing: true,
       serverSide: true,
       ordering: false,
